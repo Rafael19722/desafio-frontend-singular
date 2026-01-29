b71ad0120574c39f5b8101564d740587cbf313b2
Rafael Rangel <rafinhad3las@gmail.com>
2026-01-29 07:58:07 -0300

    feat: implement fixed header with scroll effect and logo

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,15 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Setup Concluído</h1>
+      <div className="container mx-auto px-4">
+        <h1 className="text-4xl font font-heading font-bold text-dark mt-10">
+          Teste da Página
+        </h1>
+        <p className="text-lg text-text-body mt-4">
+          Se der pra ler sem ta atrás do menu ta funcionando
+        </p>
+        <div className="h-[150vh]"></div>
+      </div>
     </main>
   );
 }
