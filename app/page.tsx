9ecda1a40c49c4571d48cf161fd325ae297ce67d
Rafael Rangel <rafinhad3las@gmail.com>
2026-01-29 21:13:40 -0300

    feat: implement process section with video card

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 
 import { Hero } from "@/components/sections/Hero";
 import { About } from "@/components/sections/About";
+import { Process } from "@/components/sections/Process";
 
 export default function Home() {
   return (
     <main >
       <Hero />
       <About />
+      <Process />
       
       <div className="h-[50vh] bg-white flex items-center justify-center">
         <p className="text-gray-400">Próximas seções virão aqui...</p>
